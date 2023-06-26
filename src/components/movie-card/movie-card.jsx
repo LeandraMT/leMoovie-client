996ac71ab8e35722ea012aba610ef7fc100929ae
LeandraMT <magantierleandra@gmail.com>
2023-06-27 00:18:09 +0200

    added bootstrap styling to the app

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Card } from "react-bootstrap";
+
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-        <Card>
+        <Card style={{ width: '18rem' }} className="h-100">
             <Card.Img variant="top" src={movie.ImagePath} />
             <Card.Body>
                 <Card.Title>{movie.Title}</Card.Title>
